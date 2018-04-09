c75939399a900e5c15258d50c9ae65f9f70938a5
richard.z <rlziii@icloud.com>
2018-04-09 17:43:37 -0400

    Fix a small user ID bug

--- js/code.js
+++ js/code.js
@@ -367,7 +367,6 @@ function getParameterByName(name, url) {
 function startPosts()
 {
     currentUserID = getParameterByName('currentUserID');
-    currentUserID = 2;
     document.getElementById("currentUserName").innerHTML = getParameterByName('username');
     populatePosts(1000);
 
